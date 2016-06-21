dd00f719a2ccbab410e738801da04d6a3ec118b9
Adam Meily <meily.adam@gmail.com>
2016-06-21 19:26:59 -0400

    add debug publish endpoint

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
 app.use("/public", express.static(__dirname + "/public"));
 app.use("/vendor", express.static(__dirname + "/bower_components"));
 
+app.use(bodyParser.json());
+
 app.get("/", function(req, res) {
   res.render("subscription");
 });
 
-app.listen(3000, function() {
+app.post("/publish", function(req, res) {
+  console.log("Publish");
+  console.log(JSON.stringify(res.body));
+  console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+  res.status(200).end();
+})
+
+app.listen(3000, "0.0.0.0", function() {
   console.log("Running app");
 });
