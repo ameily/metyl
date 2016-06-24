1ec6fc919c11f246eb82a185f58ba963be76f8ce
Adam Meily <meilya@ainfosec.com>
2016-06-24 15:20:30 -0400

    add socket.io handling of incomming events and use redis for backlog events for new page loads

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var io = require('socket.io')();
 
+
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
 app.use("/public", express.static(__dirname + "/public"));
@@ -10,19 +11,42 @@ app.use("/vendor", express.static(__dirname + "/bower_components"));
 
 app.use(bodyParser.json());
 
+var redis = require('redis').createClient({
+  host: "192.168.100.12"
+});
 
-function getNamespaceTitle(namespace) {
-  return namespace.replace(/\w\S*/g, function(txt) {
+//TODO remove me
+redis.ltrim("metasponse", 1, 0);
+
+
+function getRoomTitle(room) {
+  return room.replace(/\w\S*/g, function(txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
 
 
-app.get("/subscribe/:namespace", function(req, res) {
-  console.log("namespace: %s", req.params.namespace);
+app.get("/subscribe/:room", function(req, res) {
+  console.log("room: %s", req.params.room);
   res.render("subscription", {
-    namespace: req.params.namespace,
-    title: getNamespaceTitle(req.params.namespace)
+    room: req.params.room,
+    title: getRoomTitle(req.params.room)
+  });
+});
+
+app.get("/subscribe/:room/backlog.json", function(req, res) {
+  redis.lrange(req.params.room, 0, 999, function(err, reply) {
+    if(reply) {
+      console.log("sending backlog: %d", reply.length);
+      res.set('Content-Type', "application/json").status(200);
+      res.write('[');
+      res.write(reply.join(','));
+      res.write(']');
+      res.end();
+    } else {
+      console.log("no backlog to send: %s", err);
+      res.status(200).json([]);
+    }
   });
 });
 
@@ -33,13 +57,26 @@ app.post("/debug/publish", function(req, res) {
 
 app.post("/publish/:room", function(req, res) {
   //TODO
-  io.of('/' + req.params.room).emit('event', {
+  console.log("publish: %s", req.params.room);
+  var event = {
     origin: req.connection.remoteAddress,
     data: req.body
-  });
+  };
+
+  redis.rpush(req.params.room, JSON.stringify(event));
+  redis.ltrim(req.params.room, 0, 999);
+
+  io.in(req.params.room).emit('data', event);
 
   res.status(200).end();
 });
+
+io.on('connect', function(socket) {
+  socket.on('join', function(room) {
+    console.log("joining room: %s", room);
+    socket.join(room);
+  });
+})
 
 var server = app.listen(3000, "0.0.0.0", function() {
   console.log("Running app");
