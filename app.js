d8fb859a38da98a6a58787f50cd63dd31d6ea047
Adam Meily <meily.adam@gmail.com>
2016-06-23 23:05:48 -0400

    initial commit of socket.io handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var io = require('socket.io')(app);
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
@@ -9,8 +10,20 @@ app.use("/vendor", express.static(__dirname + "/bower_components"));
 
 app.use(bodyParser.json());
 
-app.get("/", function(req, res) {
-  res.render("subscription");
+
+function getNamespaceTitle(namespace) {
+  return namespace.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+
+
+app.get("/subscribe/:namespace", function(req, res) {
+  console.log("namespace: %s", req.params.namespace);
+  res.render("subscription", {
+    namespace: req.params.namespace,
+    title: getNamespaceTitle(req.params.namespace)
+  });
 });
 
 app.post("/debug/publish", function(req, res) {
@@ -18,8 +31,13 @@ app.post("/debug/publish", function(req, res) {
   res.status(200).end();
 });
 
-app.post("/publish", function(req, res) {
+app.post("/publish/:room", function(req, res) {
   //TODO
+  io.of('/' + req.params.room).emit('event', {
+    origin: req.connection.remoteAddress,
+    data: req.body
+  });
+
   res.status(200).end();
 })
 
